e0b259caa4a4da87804b947845320e1fa5975dae
Tran Quoc <quoctk@deha-soft.com>
2022-03-19 16:14:27 +0700

    Vẽ giao diện đăng nhập

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,9 +1,15 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { IonicModule } from "@ionic/angular";
 import { LoginRoutingModule } from "./login-routing.module";
 import { LoginPage } from './login.page';
 
 @NgModule({
     declarations: [LoginPage],
-    imports: [LoginRoutingModule]
+    imports: [
+        CommonModule,
+        IonicModule,
+        LoginRoutingModule,
+    ]
 })
 export class LoginModule {}
